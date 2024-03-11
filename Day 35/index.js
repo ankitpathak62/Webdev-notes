5aa2c7484bb1ab3b8a7e3d1477a61d0cf99ab24f
Ankit Pathak <pathakankit688@gmail.com>
2024-03-11 14:48:15 +0530

    day 35

---
+++ Day 35/index.js
@@ -0,0 +1,16 @@
+// console.log("Sunfire sensemnfbndbfbi")
+// console.log('Sunfire sensemnfbndbffghhgsds12222bi')
+
+
+const studentID = 123456
+let studentName = "Ankit";
+studentName = "Ankitaaaa";
+studentEmail = "ankit@gmail.com"
+studentContact = "123456789"
+studentContact = "123456789jkdfjdjghd"
+
+
+ 
+// console.log(studentName);
+console.table([studentName,studentEmail,studentContact])
+

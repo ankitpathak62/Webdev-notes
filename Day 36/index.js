22b90c8663bd43114d1935ef8cda882d381e88c8
Ankit Pathak <pathakankit688@gmail.com>
2024-03-12 13:41:40 +0530

    day 36

---
+++ Day 36/index.js
@@ -0,0 +1,38 @@
+"use strict";
+
+// alert("welcome to sunfire sensei")
+
+// console.log("Sunfire Sensei");
+// console.log("Sunfire Sensei")
+
+// let studentID = 120;
+// console.log(studentID);
+// console.log(typeof studentID);
+
+// let studentName ='Ankit123'
+// console.log(studentName);
+// console.log(typeof studentName);
+
+// let studentSubscribeUs = false;
+// console.log(studentSubscribeUs);
+// console.log(typeof studentSubscribeUs);
+
+// let studentSubscribeUss =true ;
+// console.log(studentSubscribeUss);
+// console.log(typeof studentSubscribeUss);
+
+// let studentSubscribe;
+// console.log(studentSubscribe);
+// console.log(typeof studentSubscribe);
+
+// let studentSubscribeUs = null;
+// // console.log(studentSubscribeUs);
+// console.log(typeof studentSubscribeUs);
+
+const student = {
+    name:"ankit"
+    reg_number : 2365
+    subscribe : true
+
+}
+consol
---
+++ Day 36/tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+let studentID = 100.53;
+// console.log(studentID);
+// console.log(typeof studentID);

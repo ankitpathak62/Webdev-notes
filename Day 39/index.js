ecc19fd8c1562c514b25fb74aaa6d5877273246c
Ankit Pathak <pathakankit688@gmail.com>
2024-03-16 20:24:30 +0530

    day 39

---
+++ Day 39/index.js
@@ -0,0 +1,44 @@
+let str = "Sunfire Sensei"
+let str1 = 'Ankit'
+let str2 = 20
+// let str2 = "Hello'
+//  <--Use opening and closing quotes are same -->
+// console.log(str);
+
+// console.log(str1.length);
+
+// console.log(str1[7]);
+
+// console.log("1Hey guys welcome to ", str ,"I am ",str1, "I am ",str2,"Years old");
+// console.log(`Hey guys welcome to ${str} I am ${str1} I am ${str2} Years old`);
+
+// console.log(`Hey guys welcome to ${str} I am ${str1} I am ${ str2+2} Years old`);
+
+// console.log(`Sum of all number is ${62*3}`);
+// let str3 = "Sunfire\nSensei"
+// console.log(str3.length);
+
+// let abc = str.toUpperCase();
+// let abcd = abc.toLowerCase();
+// console.log(str);
+// console.log(abc);
+// console.log(abcd);
+
+
+// let str4 = "       Hello Everyone     "
+// console.log(str4);
+// console.log(str4.trim());
+
+ 
+// console.log(str.slice(3,7));
+
+// console.log(str+str2+str1);
+
+// console.log(str.concat(str1))
+
+
+// console.log(str.replace("sei","win"));
+
+// console.log(str.replaceAll("e","i"));
+
+// console.log(str.charAt(6));

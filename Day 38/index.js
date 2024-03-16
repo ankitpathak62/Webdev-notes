18544779a57fdef7ad01c6b34d96feda375e4103
Ankit Pathak <pathakankit688@gmail.com>
2024-03-16 19:26:38 +0530

    day 38

---
+++ Day 38/index.js
@@ -0,0 +1,24 @@
+// let userName = "Ankit"
+
+// let newName = userName
+
+// newName = "Raja Kumar"
+
+// console.log(newName);
+// console.log(userName);
+
+let userFirst = {
+      firstName: "John",
+      lastName: "Doe",
+      age: 50,
+      eyeColor: "blue",
+    };
+
+let userSecond = userFirst 
+userFirst.eyeColor="black"
+userSecond.age=60;
+console.log(userFirst);
+console.log(userSecond);
+// console.log(userFirst);
+
+

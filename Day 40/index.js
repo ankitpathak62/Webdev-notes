f0688b24e5614ef6fe1d96555242dd0c6fe040cd
Ankit Pathak <pathakankit688@gmail.com>
2024-03-17 21:44:58 +0530

    day 40

---
+++ Day 40/index.js
@@ -0,0 +1,41 @@
+// <<<<<<<<<Number>>>>>>>>>>
+
+
+let studentMarks = 549
+// console.log(studentMarks);
+
+const balance = new Number(600)
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+// console.log(balance.toPrecision(5));
+
+let number = 100000000000;
+// console.log(number.toLocaleString('en-IN'));
+
+
+
+// <<<<<<<<<Math>>>>>>>>>>>>>
+
+// console.log(Math);
+
+// console.log(Math.abs(-55646546540));
+
+// console.log(Math.round(5.4));
+// console.log(Math.round(5.5));
+// console.log(Math.round(5.6));
+
+// console.log(Math.ceil(5.1));
+// console.log(Math.floor(5.9));
+
+// console.log(Math.sqrt(4));
+
+// console.log(Math.min(65,9,56,56,542,3,46,85));
+// console.log(Math.max(65,9,56,56,542,3,46,85));
+// console.log(Math.random());
+// console.log((Math.random()*10)+1);
+
+
+let min = 50
+let max = 100
+
+console.log(Math.ceil(Math.random() * (max-min+1))+min );
---
+++ Day 40/index1.js
@@ -0,0 +1,8 @@
+let a=10,b=10,c=10
+if (a==b==c) //a==b true 1 ==10 false
+{
+    console.log("Welcome");
+}
+else{
+    console.log("Bye Bye")
+}

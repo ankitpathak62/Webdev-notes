e2e5832a7dabfc441d141551c2756d11b5bef289
Ankit Pathak <pathakankit688@gmail.com>
2024-03-19 22:55:25 +0530

    array notes

--- Day 42/arraymethod.js
+++ Day 42/arraymethod.js
@@ -1,30 +1,38 @@
-let studentName =["Ankit","Harshita","Sunidhi","Harsh","Raja","vishal"]
-// console.log(studentName);
-// studentName.push("Rohit","Arti")
+// let studentName =["Ankit","Harshita","Sunidhi","Harsh","Raja","vishal"]
+// // console.log(studentName);
+// // studentName.push("Rohit","Arti")
 
-// let deletee = studentName.pop()
-// console.log(`delete student ${deletee}`);
-// console.log(studentName);
+// // let deletee = studentName.pop()
+// // console.log(`delete student ${deletee}`);
+// // console.log(studentName);
 
 
-// console.log(studentName.toString());
+// // console.log(studentName.toString());
+
+// // let money = [10,560,850,7000,2100,987,357,165,6545,9870]
+// // console.log(money);
+// // console.log(money.toString());
+
+// // let newArray = money.concat(studentName)
+// // console.log(newArray);
+
+// // console.log(money.unshift("666"));
+// // console.log(money.shift());
 
 // let money = [10,560,850,7000,2100,987,357,165,6545,9870]
+// // console.log(money.slice(2,5));
+// // console.log(money);
+// // money.splice(2,2,100,500)
+// money.splice()
 // console.log(money);
-// console.log(money.toString());
 
-// let newArray = money.concat(studentName)
-// console.log(newArray);
 
-// console.log(money.unshift("666"));
-// console.log(money.shift());
 
-let money = [10,560,850,7000,2100,987,357,165,6545,9870]
-// console.log(money.slice(2,5));
-// console.log(money);
-// money.splice(2,2,100,500)
-money.splice()
-console.log(money);
+
+let str = "lorem Ipsum is simply dummy text "
+let str1 = str.toUpperCase()
+console.log(str1);   
+
 
 
 

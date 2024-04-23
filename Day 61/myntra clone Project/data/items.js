ab65b37abb9c8dd4de0e49f89a20d6ea92efb745
Ankit Pathak <pathakankit688@gmail.com>
2024-04-23 13:37:47 +0530

    final project complete

--- Day 61/myntra clone Project/data/items.js
+++ Day 61/myntra clone Project/data/items.js
@@ -2,11 +2,11 @@ const items = [
     {
         id: '001',
         image: 'images/1.jpg',
-        company: 'Carlton London',
-        item_name: 'Rhodium-Plated CZ Floral Studs',
+        company: 'Nikon',
+        item_name: 'Nikon COOLPIX A300 Point & Shoot Camera 20.1 MP',
         original_price: 1045,
         current_price: 606,
-        discount_percentage: 42,
+        discount: 42,
         return_period: 14,
         delivery_date: '10 Oct 2023',
         rating: {
@@ -17,11 +17,11 @@ const items = [
     {
         id: '002',
         image: 'images/2.jpg',
-        company: 'CUKOO',
-        item_name: 'Women Padded Halter Neck Swimming Dress',
+        company: 'Blue Heaven ',
+        item_name: 'Blue Heaven Festive Fair Tone Make Up Kit - Vanilla (10Pcs)',
         original_price: 2599,
         current_price: 1507,
-        discount_percentage: 42,
+        discount: 42,
         return_period: 14,
         delivery_date: '10 Oct 2023',
         rating: {
@@ -32,11 +32,11 @@ const items = [
     {
         id: '003',
         image: 'images/3.jpg',
-        company: 'NUEVOSDAMAS',
-        item_name: 'Women Red & White Printed A-Line Knee-Length Skirts',
+        company: 'HERO',
+        item_name: 'Calcuttacycleco Hero Cycles For Mens And Boys 27.5 T Mountain/Hardtail Cycle',
         original_price: 1599,
         current_price: 495,
-        discount_percentage: 69,
+        discount: 69,
         return_period: 14,
         delivery_date: '10 Oct 2023',
         rating: {
@@ -47,11 +47,11 @@ const items = [
     {
         id: '004',
         image: 'images/4.jpg',
-        company: 'ADIDAS',
-        item_name: 'Indian Cricket ODI Jersey',
+        company: 'apple',
+        item_name: 'macbook m1',
         original_price: 999,
         current_price: 999,
-        discount_percentage: 0,
+        discount: 0,
         return_period: 14,
         delivery_date: '10 Oct 2023',
         rating: {
@@ -66,7 +66,7 @@ const items = [
         item_name: 'Pure Cotton T-shirt',
         original_price: 1399,
         current_price: 489,
-        discount_percentage: 65,
+        discount: 65,
         return_period: 14,
         delivery_date: '10 Oct 2023',
         rating: {
@@ -77,11 +77,11 @@ const items = [
     {
         id: '006',
         image: 'images/6.jpg',
-        company: 'Nike',
-        item_name: 'Men ReactX Running Shoes',
+        company: 'watch',
+        item_name: 'Men ReactX watch',
         original_price: 14995,
         current_price: 14995,
-        discount_percentage: 0,
+        discount: 0,
         return_period: 14,
         delivery_date: '10 Oct 2023',
         rating: {
@@ -96,7 +96,7 @@ const items = [
         item_name: 'Men Slim Fit Regular Shorts',
         original_price: 1599,
         current_price: 639,
-        discount_percentage: 60,
+        discount: 60,
         rating: {
             stars: 4.2,
             count: 388,
@@ -109,7 +109,7 @@ const items = [
         item_name: 'Men Fresh Deodrant 150ml',
         original_price: 285,
         current_price: 142,
-        discount_percentage: 50,
+        discount: 50,
         return_period: 14,
         delivery_date: '10 Oct 2023',
         rating: {

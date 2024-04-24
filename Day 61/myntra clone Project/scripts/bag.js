bc8b2491088a10cf1d7326202def4ef998bc5327
Ankit Pathak <pathakankit688@gmail.com>
2024-04-24 08:05:49 +0530

    myntra

--- Day 61/myntra clone Project/scripts/bag.js
+++ Day 61/myntra clone Project/scripts/bag.js
@@ -5,10 +5,52 @@ onLoad();
 function onLoad() {
   loadBagItemObjects();
   displayBagItems();
+  displayBagSummary()
 
 }
 
 
+function displayBagSummary() {
+  let bagSummaryElement = document.querySelector(".bag-summary")
+
+  let totalItem = 0
+  let totalMRP = 0
+  let totalDiscount = 0
+
+
+  bagItemObjects.forEach(bagItem => {
+    totalMRP += bagItem.original_price;
+    totalDiscount += bagItem.original_price - bagItem.current_price
+  })
+
+  let totalamount = totalMRP - totalDiscount + 99
+
+  bagSummaryElement.innerHTML = `
+    <div class="bag-details-container">
+    <div class="price-header">PRICE DETAILS (${totalItem} Items) </div>
+    <div class="price-item">
+      <span class="price-item-tag">Total MRP</span>
+      <span class="price-item-value">₹${totalMRP}</span>
+    </div>
+    <div class="price-item">
+      <span class="price-item-tag">Discount on MRP</span>
+      <span class="price-item-value priceDetail-base-discount">-₹${totalDiscount}</span>
+    </div>
+    <div class="price-item">
+      <span class="price-item-tag">Convenience Fee</span>
+      <span class="price-item-value">₹99</span>
+    </div>
+    <hr>
+    <div class="price-footer">
+      <span class="price-item-tag">Total Amount</span>
+      <span class="price-item-value">₹${totalamount}</span>
+    </div>
+  </div>
+  <button class="btn-place-order">
+    <div class="css-xjhrni">PLACE ORDER</div>
+  </button>
+  `;
+}
 
 function loadBagItemObjects() {
   console.log(bagItems);
@@ -31,7 +73,15 @@ function displayBagItems() {
   containerElement.innerHTML = innerHTML;
 }
 
+function removeFromBag(itemId) {
+  bagItems = bagItems.filter(bagItemId => bagItemId != itemId);
+  localStorage.setItem('bagItems', JSON.stringify(bagItems));
+  loadBagItemObjects();
+  displayBagIcon();
+  displayBagItems();
+  displayBagSummary()
 
+}
 
 function generateItemHTML(item) {
   return `<div class="bag-item-container">

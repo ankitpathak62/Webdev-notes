358c5e09024c6c1086718d8eb56f8352b005841b
Ankit Pathak <pathakankit688@gmail.com>
2024-05-02 20:58:46 +0530

    day63 day 62

---
+++ Day 62/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*index.html"],
+  theme: {
+    extend: {
+      spacing: {
+        'w-7.5': 'width: 75.6px',
+      }
+    },
+  },
+  plugins: [],
+}
+

1c441826a7a8a4da8911326b9ab65283903e30fc
Ankit Pathak <pathakankit688@gmail.com>
2024-05-15 13:55:55 +0530

    Day 72

--- Day-72/src/App.jsx
+++ Day-72/src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import { useRef } from 'react'
- 
- 
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import { useRef } from "react";
+
 // const refObject = useRef(initial value)
 function App() {
-  const[Name,setName] =useState(" ")
-  const refObject = useRef()
-  console.log(refObject)
+  const [Name, setName] = useState(" ");
+  const refObject = useRef();
+  console.log(refObject);
 
   function Reset() {
-    setName("")
-    refObject.current.focus()
+    setName("");
+    refObject.current.focus();
   }
   function Default() {
-    refObject.current.value= "Ankit"
-    refObject.current.focus()
+    refObject.current.value = "Ankit";
+    refObject.current.focus();
   }
-
+  
   return (
     <>
-       
       <h1>Sunfire Sensei</h1>
-       <input ref={refObject} type='text' value={Name} onChange={(event) => setName(event.target.value)}></input>
-       <button onClick={Reset}>Reset</button>
-       <button onClick={Default}>Default</button>
-       
-      
+      <input
+        ref={refObject}
+        type="text"
+        value={Name}
+        onChange={(event) => setName(event.target.value)}
+      ></input>
+      <button onClick={Reset}>Reset</button>
+      <button onClick={Default}>Default</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

c73c90797ffca5fe9aa2a33aeece6c4c4862ea66
Ankit Pathak <pathakankit688@gmail.com>
2024-05-23 06:30:43 +0530

    Day -76

--- Day-76/memoeffect/src/App.jsx
+++ Day-76/memoeffect/src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useMemo, useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [sum, setsum] = useState(0);
+  const [minus, setminus] = useState(1000);
+  const multi = useMemo(function multi() {
+    console.log("Hello this is your sensei Ankit ");
+    return sum * 5;
+  }, [minus]);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Sunfire Sensei</h1>
+      {multi}
+      <br></br>
+      <button onClick={() => setsum(sum + 1)}>+</button>
+      <span>{sum}</span>
+      <br></br>
+      <button onClick={() => setminus(minus - 1)}>-</button>
+      <span>{minus}</span>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ Day-76/memoeffect/src/function.js
@@ -0,0 +1,5 @@
+function add(a, b) {
+  return a + b;
+}
+add(3, 9);
+add(3, 9);

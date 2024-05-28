16a83ab3d53d533e355f48d5cf074b4a752faad3
Ankit Pathak <pathakankit688@gmail.com>
2024-05-28 10:09:32 +0530

    Day 79

--- Day-79/src/App.jsx
+++ Day-79/src/App.jsx
@@ -7,11 +7,13 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 const App = () => {
   return (
-    <Router>
+    <>
       <div>
         <nav className="   mb-20 flex items-center justify-between py-6">
           <div className="  flex flex-shrink-0 items-center text-4xl">
-            <Link to="RB" className="mx-2 w-10">RB</Link>
+            <Link to="RB" className="mx-2 w-10">
+              RB
+            </Link>
           </div>
           <div
             className="m-8 flex items-center justify-center gap-4 text-2xl
@@ -23,12 +25,14 @@ const App = () => {
         </nav>
 
         <Routes>
+          <Route path="/" element={<h1>hello this is Ankit</h1>}></Route>
           <Route path="/Experience" element={<Experience />} />
           <Route path="/RB" element={<Hero />} />
+          {/* <Route path="/" element={<Hero />} /> */}
           <Route path="/Project" element={<Project />} />
         </Routes>
       </div>
-    </Router>
+    </>
   );
 };
 

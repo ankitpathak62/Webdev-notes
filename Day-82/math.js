0625e5fd149bb19056f2f291ed1f76b50e7fbc13
Ankit Pathak <pathakankit688@gmail.com>
2024-06-09 17:10:18 +0530

    day 82

---
+++ Day-82/math.js
@@ -0,0 +1,15 @@
+// function add(a, b) {
+//   return a + b;
+// }
+
+// function sub(a, b) {
+//   return a - b;
+// }
+
+// module.exports = {
+//    add,
+//    sub,
+// };
+
+exports.add = (a, b) => a + b;
+exports.sub = (a, b) => a - b;
---
+++ Day-82/test.js
@@ -0,0 +1,8 @@
+const math = require("./math");
+
+const math1 = require("fs");
+const math2 = require("http");
+
+// console.log(math.add(6,8));
+console.log(math1);
+console.log(math2);

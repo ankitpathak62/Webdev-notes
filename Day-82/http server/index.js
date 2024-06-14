0b81f4ab9a27d32481fdb79251e313c6d1055919
Ankit Pathak <pathakankit688@gmail.com>
2024-06-14 13:46:25 +0530

    server

--- Day-82/http server/index.js
+++ Day-82/http server/index.js
@@ -2,22 +2,23 @@ const http = require("http");
 const fs = require("fs");
 
 const myServer = http.createServer((req, res) => {
-    const log = `${Date.now()}: ${req.url} New Req Received\n`;
-    fs.appendFile("log.txt", log, (err) => {
-        if (err) console.error(err);
-    }); 
+  if (req.url === "/favicon.ico") return res.end();
+  const log = `${Date.now()}: ${req.url} New Request\n`;
+  fs.appendFile("request.txt", log, (err) => {
+    if (err) console.error(err);
+  });
 
-    switch (req.url) {
-        case "/":
-            res.end("Welcome to youtube");
-            break;
-        case "/channel":
-            res.end("Sunfire Sensei");
-            break;
-        default:
-            res.statusCode = 404;
-            res.end("404 Not Found");
-    }
+  switch (req.url) {
+    case "/":
+      res.end("Welcome to youtube");
+      break;
+    case "/channel":
+      res.end("Sunfire Sensei");
+      break;
+    default:
+      res.statusCode = 404;
+      res.end("404 Not Found");
+  }
 });
 
 myServer.listen(8000, () => console.log("Server Started!"));

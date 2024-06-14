ce677871ec44d221505fdcbbce3646a525ec0089
Ankit Pathak <pathakankit688@gmail.com>
2024-06-14 13:41:49 +0530

    server

--- Day-82/http server/index.js
+++ Day-82/http server/index.js
@@ -5,7 +5,7 @@ const myServer = http.createServer((req, res) => {
     const log = `${Date.now()}: ${req.url} New Req Received\n`;
     fs.appendFile("log.txt", log, (err) => {
         if (err) console.error(err);
-    });
+    }); 
 
     switch (req.url) {
         case "/":

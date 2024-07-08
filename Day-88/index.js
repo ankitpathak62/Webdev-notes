f63dc7e8cdafaf25e69d9054c41c4e388ade5acb
Ankit Pathak <pathakankit688@gmail.com>
2024-07-08 05:39:57 +0530

    Day 88

--- Day-88/index.js
+++ Day-88/index.js
@@ -8,28 +8,29 @@ const PORT = 3500;
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
-// const upload = multer({ dest: "uploads/" });
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     return cb(null, "./uploads");
   },
+
   filename: function (req, file, cb) {
-    return cb(null, `${Date.now} -${file.originalname}`);
+    return cb(null, ` ${file.originalname}`);
   },
 });
 const upload = multer({ storage: storage });
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.get("/", (req, res) => {
-  return res.render("HomePage");
+  return res.render("Homepage");
 });
 
 app.post("/upload", upload.single("Img"), (req, res) => {
-  console.log(req.body);
-  console.log(req.file);
-
+  console.log(res.body);
+  console.log(res.file);
   return res.redirect("/");
 });
 
-app.listen(PORT, () => console.log(`Server Start`));
+app.listen(PORT, () => {
+  console.log(`Server Start`);
+});

7554da3daac282efa09e61fc721a1f5aba644c71
Ankit Pathak <pathakankit688@gmail.com>
2024-08-15 14:55:01 +0530

    Backend JWT Token complete

--- Day-97/ChatApp/Backend/controller/user.controller.js
+++ Day-97/ChatApp/Backend/controller/user.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import createTokenAndSaveCookie from "../jwt/generateToken.js";
 
 export const signup = async (req, res) => {
   try {
@@ -19,13 +20,50 @@ export const signup = async (req, res) => {
       email,
       password: hashedPassword,
     });
-    await newUser
-      .save()
-      .then(() =>
-        res.status(201).json({ message: "User registered successfully" })
-      );
+    await newUser.save();
+    if (newUser) {
+      createTokenAndSaveCookie(newUser._id, res);
+      res
+        .status(201)
+        .json({ message: "User registered successfully", newUser });
+    }
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "Server error" });
   }
 };
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!user || !isMatch) {
+      return res.status(404).json({ message: "invalid User or Password" });
+    }
+    createTokenAndSaveCookie(user._id, res);
+    res.status(201).json({
+      message: "User logged in successfully",
+      user: {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+
+export const logout = async(req,res)=>{
+  try {
+    res.clearCookie('jwt');
+    res.status(200).json({ message: "User logged out successfully" });
+    
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Server error" });
+  }
+}
---
+++ Day-97/ChatApp/Backend/jwt/generateToken.js
@@ -0,0 +1,14 @@
+import jwt from "jsonwebtoken";
+
+const createTokenAndSaveCookie = (userId, res) => {
+  const token = jwt.sign({ userId }, process.env.JWT_TOKEN, {
+    expiresIn: "5d",
+  });
+  res.cookie("jwt", token, {
+    httpOnly: true, //xss
+    secure: true,
+    sameSite: "strict", //csrf
+  });
+};
+
+export default createTokenAndSaveCookie;

ec2b062e98b617d241d74e9c6f2add6b9c6057a7
Ankit Pathak <pathakankit688@gmail.com>
2024-08-27 09:55:10 +0530

    message work

--- Day-97/ChatApp/Frontend/src/home/right/Right.jsx
+++ Day-97/ChatApp/Frontend/src/home/right/Right.jsx
@@ -1,23 +1,48 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Chatuser from "./Chatuser";
 import Messages from "./Messages";
 import Type from "./Type";
 import useConversation from "../../statemanage/useConversation.js";
+import { useAuth } from "../../context/AuthProvider.jsx";
 
-export default function Right() {
+function Right() {
   const { selectedConversation, setSelectedConversation } = useConversation();
+  useEffect(() => {
+    return setSelectedConversation(null);
+  }, [setSelectedConversation]);
+  return (
+    <div className="w-full bg-slate-800 text-gray-300">
+      <div>
+        {!selectedConversation ? (
+          <NoChat />
+        ) : (
+          <>
+            <Chatuser />
+            <div
+              className=" flex-1 overflow-y-auto"
+              style={{ maxHeight: "calc(88vh - 8vh)" }}
+            >
+              <Messages />
+            </div>
+            <Type />
+          </>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Right;
+
+const NoChat = () => {
+  const [authUser] = useAuth();
+  console.log(authUser);
   return (
     <>
-      <div className="w-[70%]   bg-slate-950 text-white">
-        <Chatuser></Chatuser>
-        <div
-          className="py-2 flex-ankit overflow-y-auto"
-          style={{ maxHeight: "calc(88vh - 8vh)" }}
-        >
-          <Messages></Messages>
-        </div>
-        <Type></Type>
+      <div className="flex h-screen items-center justify-center">
+        <h1 className="font-semibold text-xl">Welcome <span>{authUser.user.name}</span>
+          <br></br>Select a chat to start messaging.</h1>
       </div>
     </>
   );
-}
+};
